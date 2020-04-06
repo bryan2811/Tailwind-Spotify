8da958ae4c9dc41529f87d2e85cd20bbfe46dc3a
Bryan Silva <brandonb2816@gmail.com>
2020-04-06 12:33:13 -0300

    Empezando Layout

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'gray-900-spotify': '#121212',
+        'gray-800-spotify': '#181818',
+        'gray-700-spotify': '#282828'
+      },
+      lineHeight: {
+        'extra-loose': '2.5',
+      }
+    },
+  },
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
+  plugins: [],
+}
